8540e29de245fb2f9000582b7564087529bed31c
Tradingcode-create <73110868+Tradingcode-create@users.noreply.github.com>
2026-05-16 01:59:11 +0200

    settings.py

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-change-me-before-production"
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
 ALLOWED_HOSTS = [
     ".onrender.com",
     "localhost",
